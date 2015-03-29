8facbd56f1d33525f3ea52ca73f18e1ab20c5518
Christian Hausknecht <christian.hausknecht@gmx.de>
2015-03-29 18:24:28 +0200

    4Clojure problem 27
    Solved problem 27 palindorme detection

--- Clojure/4clojure.clj
+++ Clojure/4clojure.clj
@@ -1,6 +1,14 @@
+;;
 ;; problem 26
 ;; Fibonacci Sequence
 #(loop [n % acc [1 1]]
-	(if (> n 2)
-		(recur (dec n) (conj acc (apply + (take-last 2 acc))))
-		acc))
+  (if (> n 2)
+      (recur (dec n) (conj acc (apply + (take-last 2 acc))))
+  acc))
+
+;;
+;; problem 27
+;; Palindrome detection
+#(let [v (vec %) 
+	   rv (reverse (vec %))]
+  (every? true? (map = v rv)))
